71cf570cc88ca52701d70d463574f3d56c2b84ae
LuisMunar <luis.emunarr@gmail.com>
2022-05-07 08:33:26 -0500

    Endpoint to get driver was fixes

--- src/app/services/driversService.js
+++ src/app/services/driversService.js
@@ -4,7 +4,6 @@ import apiConfig from '../config/apiConfig'
 export const getDriverService = (diverId) => {
   return new Promise(async (resolve) => {
     const { data:{ result } } = await httpGetMethod(apiConfig.driversEndpoint, { id: diverId })
-    const { rows } = result
-    resolve(rows.length>0 ? rows[0] : {})
+    resolve(typeof(result) === 'string' ? {} : result)
   })
 }
